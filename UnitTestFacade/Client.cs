9c8d3cc55fc5d7d5c25e9e3f87f4150ffce21c51
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2026-05-27 23:29:51 -0600

    Design Pattern: Facade

---
+++ UnitTestFacade/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTestFacade
+{
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            UnitTestKeeper unitTestKeeper = new UTKImplementation();
+
+            AVLUnitTest avlUnitTest = unitTestKeeper.getAVLTests();
+            BSTUnitTest bstUnitTest = unitTestKeeper.getBSTTests();
+        }
+    }
+}
---
+++ UnitTestFacade/Runners.cs
@@ -0,0 +1,53 @@
+﻿using TreeLibrary;
+using Xunit;
+
+namespace UnitTestFacade
+{
+    public interface Runner
+    {
+        public UnitTest getTests();
+    }
+
+    public class BSTUnitTester : Runner
+    {
+        public UnitTest getTests()
+        {
+            BSTUnitTest bstTest = new BSTUnitTest();
+            bstTest.addData(new List<int> { 5, 4, 3, 2, 1 });
+            bstTest.populateTree();
+
+            Node_BST<int> nextNode = bstTest.tree.Root;
+            for (int i = bstTest.tree.Count; i > 0; i--)
+            {
+                Assert.Equal(i, nextNode.Data);
+                nextNode = nextNode.Left;
+            }
+
+            return bstTest;
+        }
+    }
+
+    public class AVLUnitTester : Runner
+    {
+        public UnitTest getTests()
+        {
+            AVLUnitTest avlTest = new AVLUnitTest();
+            avlTest.addData(new List<int> { 5, 4, 3, 2, 1 });
+            avlTest.populateTree();
+
+            TreePrinter.PrintMyTree(avlTest.tree);
+
+            //Root
+            Assert.Equal(4, avlTest.tree.Root.Data);
+            //Left
+            Assert.Equal(2, avlTest.tree.Root.Left.Data);
+            Assert.Equal(1, avlTest.tree.Root.Left.Left.Data);
+            Assert.Equal(3, avlTest.tree.Root.Left.Right.Data);
+            //Right
+            Assert.Equal(5, avlTest.tree.Root.Right.Data);
+
+
+            return avlTest;
+        }
+    }
+}
